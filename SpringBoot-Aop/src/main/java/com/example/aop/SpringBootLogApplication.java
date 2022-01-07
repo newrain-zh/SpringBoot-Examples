7f793666083467d8915f943e5ad5ed26080af2b4
newrain-zh <496268609@qq.com>
2022-01-07 14:07:05 +0800

    feat(SpringBoot-Examples): SpringBoot 使用示例
    SpringBoot Aop中切面日志示例

---
+++ SpringBoot-Aop/src/main/java/com/example/aop/SpringBootLogApplication.java
@@ -0,0 +1,18 @@
+package com.example.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author newRain
+ * @description 启动类
+ */
+@SpringBootApplication
+public class SpringBootLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLogApplication.class, args);
+    }
+
+
+}
