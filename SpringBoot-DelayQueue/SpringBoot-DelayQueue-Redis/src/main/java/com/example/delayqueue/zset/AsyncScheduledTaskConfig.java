452a42ed37b1a967332a29f3295a7145bb98cd9f
newrain-zh <496268609@qq.com>
2022-01-13 16:45:50 +0800

    feat(SpringBoot-Examples): Redis延时队列实现
    Redis zset实现延时队列代码

---
+++ SpringBoot-DelayQueue/SpringBoot-DelayQueue-Redis/src/main/java/com/example/delayqueue/zset/AsyncScheduledTaskConfig.java
@@ -0,0 +1,79 @@
+package com.example.delayqueue.zset;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author newRain
+ * @description 线程池配置
+ */
+@Component
+@EnableAsync
+@Slf4j
+@Configuration
+public class AsyncScheduledTaskConfig implements SchedulingConfigurer, AsyncConfigurer {
+
+    /**
+     * 定时任务使用的线程池
+     *
+     * @return
+     */
+    @Bean(destroyMethod = "shutdown", name = "taskScheduler")
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(10);
+        scheduler.setThreadNamePrefix("task-");
+        scheduler.setAwaitTerminationSeconds(600);
+        scheduler.setWaitForTasksToCompleteOnShutdown(true);
+        return scheduler;
+    }
+
+    /**
+     * 异步任务执行线程池
+     *
+     * @return
+     */
+    @Bean(name = "asyncExecutor")
+    public ThreadPoolTaskExecutor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setQueueCapacity(1000);
+        executor.setKeepAliveSeconds(600);
+        executor.setMaxPoolSize(20);
+        executor.setThreadNamePrefix("asyncExecutor-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        ThreadPoolTaskScheduler taskScheduler = taskScheduler();
+        scheduledTaskRegistrar.setTaskScheduler(taskScheduler);
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        return asyncExecutor();
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (throwable, method, objects) -> {
+            log.error("异步任务执行出现异常, message {}, emthod {}, params {}", throwable, method, objects);
+        };
+    }
+
+}
---
+++ SpringBoot-DelayQueue/SpringBoot-DelayQueue-Redis/src/main/java/com/example/delayqueue/zset/DelayQueueJob.java
@@ -0,0 +1,88 @@
+package com.example.delayqueue.zset;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Set;
+
+/**
+ * 延迟队列
+ *
+ * @author newRain
+ * @description Redis zSet 延迟队列实现
+ */
+@Component
+@Slf4j
+public class DelayQueueJob {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public static final String DELAY_KEY = "DELAY_QUEUE_TEST";
+
+    @Scheduled(fixedRate = 1000)
+    public void scheduledTask() {
+        //当前时间
+        long now = System.currentTimeMillis();
+        long before = now - 10000;
+        log.info("before end:{} {}", longToString(before), longToString(now));
+        Set<String> strings = stringRedisTemplate.opsForZSet().rangeByScore(DELAY_KEY, before, now);
+        if (CollectionUtils.isEmpty(strings)) {
+            return;
+        }
+        //单个服务的情况可以如下实现
+        strings.parallelStream().forEach(member -> {
+            log.info("process member:{} time:{}", member, longToString(System.currentTimeMillis()));
+            //在分布式环境下 例如本服务有多副本机制则要考虑多线程情况下幂等情况
+            //最简单的如下进行删除操作 失败进行记录和重试、补偿手段
+            //业务处理中若有数据库也可以用数据库的记录来保障幂等处理
+            Long count = stringRedisTemplate.opsForZSet().remove(DELAY_KEY, member);
+            if (count != null && count > 0) {
+                try {
+                    //do something
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    log.error("save fail record");
+                }
+            }
+        });
+        //单台服务的情况下，可以使用批量删除提高性能
+//      stringRedisTemplate.opsForZSet().remove(DELAY_KEY, strings.toArray());
+    }
+
+    /**
+     * 入列
+     */
+    @Scheduled(fixedRate = 3000)
+    public void offerDelayQueue() {
+        long currentTimeMillis = System.currentTimeMillis();
+        String dateTime = longToString(currentTimeMillis);
+        //score为当前时间+延时时间
+        stringRedisTemplate.opsForZSet().add(DELAY_KEY, dateTime, currentTimeMillis + 10000);
+    }
+
+    /**
+     * 时间格式化 便于日志查看
+     *
+     * @param timestamp
+     * @return
+     */
+    private static String longToString(long timestamp) {
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
+        return localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longToString(System.currentTimeMillis()));
+    }
+}
