da1441ddf1fbb1afc6cdb339d95af86f2325dd8e
zhiqin.zhang <zzqnobody@126.com>
2022-01-25 10:48:05 +0800

    feat(SpringBoot-Kafka): SpringBoot kafka集成

---
+++ SpringBoot-mq/SpringBoot-Kafka/src/main/java/com/example/kafka/controller/KafkaConsumer.java
@@ -0,0 +1,20 @@
+package com.example.kafka.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class KafkaConsumer {
+
+    @KafkaListener(topics = {"myKafka"}, groupId = "zhTestGroup")
+    public void onMessage1(ConsumerRecord<?, ?> record, Acknowledgment ack) {
+        // 消费的哪个topic、partition的消息,打印出消息内容
+        log.info("简单消费 topic={} partition={} content={}", record.topic(), record.partition(), record.value());
+        ack.acknowledge();
+    }
+
+}
