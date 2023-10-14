3937c86b0cddba064ad2d75ad37208aece9d9776
alex <zzqnobody@126.com>
2023-10-15 00:59:01 +0800

    springboot>=2.0版本使用Redis示例

---
+++ redis-example/src/main/java/com/example/redis/RedisExampleApplication.java
@@ -0,0 +1,18 @@
+package com.example.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author newRain
+ * @description 启动类
+ */
+@SpringBootApplication
+public class RedisExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisExampleApplication.class, args);
+    }
+
+
+}
