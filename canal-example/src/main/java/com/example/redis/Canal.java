9d7056e0846577216334e3382e536107bc49eb82
alex <zzqnobody@126.com>
2023-10-15 01:02:50 +0800

    canal使用示例

--- canal-example/src/main/java/com/example/redis/Canal.java
+++ canal-example/src/main/java/com/example/redis/Canal.java
@@ -1,4 +1,4 @@
-package com.example.aop;
+package com.example.redis;
 
 import com.alibaba.otter.canal.client.CanalConnector;
 import com.alibaba.otter.canal.client.CanalConnectors;
--- canal-example/src/main/java/com/example/redis/controller/LogController.java
+++ canal-example/src/main/java/com/example/redis/controller/LogController.java
@@ -1,4 +1,4 @@
-package com.example.aop.controller;
+package com.example.redis.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
